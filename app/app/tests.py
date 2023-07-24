efe7ff179f44751bf4f5f7afd5a31905cc02d6c4
Vishnu Deshmukh <vishnudeshmukh1009@gmail.com>
2023-07-25 00:37:32 +0530

    Added Unit Test and Migrations

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,11 +1,12 @@
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
     def test_calc(self):
-        res=calc.add(5,6)
-        self.assertEqual(res,11)
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
 
     def test_subtract(self):
-        res=calc.subtract(11,5)
-        self.assertEqual(res,6)
+        res = calc.subtract(11, 5)
+        self.assertEqual(res, 6)
